3b0123c666b5a4b9f0ad5691e38e10a60679b20d
Adam Jimenez <outlandico@gmail.com>
2024-05-08 23:45:10 -0700

    finished fixing database and adding tldr

--- src/models/entertainment.model.js
+++ src/models/entertainment.model.js
@@ -1,6 +1,5 @@
 'use strict';
 
-
 const Entertainment = (sequelize, DataTypes) => sequelize.define('Entertainment', {
   id: {
     type: DataTypes.INTEGER,
@@ -15,32 +14,12 @@ const Entertainment = (sequelize, DataTypes) => sequelize.define('Entertainment'
     type: DataTypes.STRING,
     allowNull: false,
   },
-  price: {
-    type: DataTypes.FLOAT,
+  location: {
+    type: DataTypes.STRING,
     allowNull: false,
   },
-  calories: {
-    type: DataTypes.INTEGER,
-    allowNull: false,
-  },
-  protein: {
-    type: DataTypes.FLOAT,
-    allowNull: false,
-  },
-  fat: {
-    type: DataTypes.FLOAT,
-    allowNull: false,
-  },
-  carbohydrates: {
-    type: DataTypes.FLOAT,
-    allowNull: false,
-  },
-  entertainmentid: {
-    type: DataTypes.INTEGER,
-
-  },
+}, {
+  tableName: 'Entertainment', // Explicitly specify the table name
 });
-
-
 
 module.exports = Entertainment;
--- src/models/food.model.js
+++ src/models/food.model.js
@@ -1,43 +1,28 @@
 'use strict';
 
-const Food = (sequelize, DataTypes) => sequelize.define('Food', {
-  id: {
-    type: DataTypes.INTEGER,
-    primaryKey: true,
-    autoIncrement: true,
-  },
-  name: {
-    type: DataTypes.STRING,
-    allowNull: false,
-  },
-  type: {
-    type: DataTypes.STRING,
-    allowNull: false,
-  },
-  price: {
-    type: DataTypes.FLOAT,
-    allowNull: false,
-  },
-  calories: {
-    type: DataTypes.INTEGER,
-    allowNull: false,
-  },
-  protein: {
-    type: DataTypes.FLOAT,
-    allowNull: false,
-  },
-  fat: {
-    type: DataTypes.FLOAT,
-    allowNull: false,
-  },
-  carbohydrates: {
-    type: DataTypes.FLOAT,
-    allowNull: false,
-  },
-  entertainmentId: {
-    type: DataTypes.INTEGER,
-    
-  },
-});
+const Food = (sequelize, DataTypes) => {
+  const Food = sequelize.define('Food', {
+    id: {
+      type: DataTypes.INTEGER,
+      primaryKey: true,
+      autoIncrement: true,
+    },
+    name: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    type: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    entertainmentId: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+    },
+  });
+
+  return Food;
+};
 
 module.exports = Food;
+
